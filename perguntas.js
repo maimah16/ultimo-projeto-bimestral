6a9df2dcb21a0e809b10d3bdc70aa2cd7f59eacd
Maira Cipriano <00001124861324sp@al.educacao.sp.gov.br>
2024-11-06 10:55:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    ' O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o "Pai da Aviação"?',
+    ' Santos Dumont, inventor brasileiro que voou publicamente com o 14-Bis em 1906.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa "HTML"?',
+    ' HTML significa "HyperText Markup Language", a linguagem padrão para criar páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O Oceano Pacífico, que cobre cerca de um terço da superfície terrestre.'
+)
+
+criaCartao(
+    'Física',
+    'O que é gravidade?',
+    'Gravidade é a força que atrai objetos uns aos outros, mantendo-nos no chão.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual gás as plantas absorvem para fazer fotossíntese?',
+    'Elas absorvem dióxido de carbono (CO₂) e liberam oxigênio.'
+)
